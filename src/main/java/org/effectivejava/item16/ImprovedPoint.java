2578efad93e63d58aed1c7d6849e9dd98355c20e
mingyu.k <mingyu.k@navercorp.com>
2022-08-10 08:19:38 +0900

    Item 16

---
+++ src/main/java/org/effectivejava/item16/ImprovedPoint.java
@@ -0,0 +1,27 @@
+package org.effectivejava.item16;
+
+public class ImprovedPoint {
+	private double x;
+	private double y;
+
+	public ImprovedPoint(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+}
---
+++ src/main/java/org/effectivejava/item16/Point.java
@@ -0,0 +1,6 @@
+package org.effectivejava.item16;
+
+public class Point {
+	public double x;
+	public double y;
+}
